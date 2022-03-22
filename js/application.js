790044998b85823ad25cb4f1b495e087a65dca60
Gustavo Rangel <gustavorangel91@gmail.com>
2022-03-22 18:09:39 -0300

    Create add item feature.

--- js/application.js
+++ js/application.js
@@ -3,7 +3,7 @@ let updateTable = function () {
 
   $("tbody tr").each(function (__, elem) {
     let price = parseFloat($(elem).children(".price").text());
-    let qty = parseInt($(elem).children(".qty").text());
+    let qty = parseInt($(elem).children(".qty").val());
 
     let subtotal = price * qty;
     $(elem).children(".subtotal").html(subtotal);
@@ -19,5 +19,29 @@ $(document).ready(function () {
   $(".btn.remove").on("click", function (__) {
     $(this).closest("tr").remove();
     updateTable();
+  });
+
+  $("#addItem").on("submit", function (event) {
+    event.preventDefault();
+    let name = $(this).children("[name=name]").val();
+    let price = $(this).children("[name=price]").val();
+    let qty = $(this).children("[name=qty]").val();
+    console.log(price);
+
+    $("tbody").append(
+      "<tr>" +
+        '<td class="name">' +
+        name +
+        "</td>" +
+        '<td class="price">' +
+        price +
+        "</td>" +
+        '<td class="qty"><input type="number" value="' +
+        qty +
+        '" /></td>' +
+        '<td class="subtotal"></td>' +
+        '<td><button class="btn btn-light btn-sm remove">Remove</button></td>' +
+        "</tr>"
+    );
   });
 });
